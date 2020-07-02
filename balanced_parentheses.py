c9dc4593d8ff463a314d94a32042c41baf94e7b0
halo-olah <a378691570@163.com>
2020-07-02 21:40:19 +0800

    修改栈的学习代码

--- balanced_parentheses.py
+++ balanced_parentheses.py
@@ -2,28 +2,35 @@
 
 class Stack(object):
     def __init__(self,limit=10):
+        # 存放元素
         self.stack = []
+        # 栈容量极限
         self.limit = limit
 
     def push(self,data):
+        # 判断栈容量是否超出范围
         if len(self.stack) >= self.limit:
-            raise IndexError("chaochurongliang")
+            raise IndexError("超出栈容量极限")
         self.stack.append(data)
 
     def pop(self):
         if self.stack:
             return self.stack.pop()
         else:
+            # 空栈不能弹出元素
             raise IndexError("pop from a empty stack")
 
     def peek(self):
+        # 查看栈顶元素
         if self.stack:
             return self.satck[-1]
 
     def is_empty(self):
+        # 判断栈是否为空
         return not bool(self.stack)
 
     def size(self):
+        # 返回栈的大小
         return len(self.stack)
 
 def balanced_parentheses(parentheses):
